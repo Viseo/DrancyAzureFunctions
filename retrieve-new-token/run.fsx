b3e94d780aa26bdf36dc1ea7c053bbfc8fd9186d
allan.menoret <allan.menoret@gmail.com>
2017-03-14 15:50:19 +0100

    add the push of the token on the queue

--- retrieve-new-token/run.fsx
+++ retrieve-new-token/run.fsx
@@ -17,7 +17,7 @@ open Microsoft.Azure.WebJobs
 let bodySample = """{"token_type":"Bearer","scope":"Calendars.Read","expires_in":"3599","ext_expires_in":"0","expires_on":"1489416293","not_before":"1489412393","resource":"https://graph.microsoft.com","access_token":"ey_-9Nv_","refresh_token":"AQ-0_"}"""
 type OAuth2Token = JsonProvider<bodySample>
 
-let Run(myTimer: TimerInfo, log: TraceWriter) =
+let Run(myTimer: TimerInfo, tokenQueue: byref<string>, log: TraceWriter) =
     let appSettings = ConfigurationManager.AppSettings 
     let url = appSettings.["TokenUrl"]
     let postInformation = "grant_type=" + appSettings.["GrantType"]
@@ -31,5 +31,5 @@ let Run(myTimer: TimerInfo, log: TraceWriter) =
     log.Info(sprintf "Message sent: %s" postInformation)
 
     let token = Http.RequestString(url, headers = [ ContentType HttpContentTypes.FormValues ], body = TextRequest postInformation) |> OAuth2Token.Parse  
-    
+    tokenQueue <- token.AccessToken
     log.Info(sprintf "token: %s" token.AccessToken)
