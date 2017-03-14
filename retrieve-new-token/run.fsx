2a294b4dcf4cecf7803e3d90b7ebb4507e7adc8e
allan.menoret <allan.menoret@gmail.com>
2017-03-14 11:00:05 +0100

    add system configuration and clean code

--- retrieve-new-token/run.fsx
+++ retrieve-new-token/run.fsx
@@ -1,5 +1,6 @@
 
 #if !COMPILED
+#r "System.Configuration.dll"
 #r "../packages/FSharp.Data.dll"
 #r "../packages/Microsoft.Azure.WebJobs.Host.dll"
 #r "../packages/Microsoft.Azure.WebJobs.Extensions.dll"
@@ -14,7 +15,7 @@ open Microsoft.Azure.WebJobs
 
 [<Literal>]
 let bodySample = """{"token_type":"Bearer","scope":"Calendars.Read","expires_in":"3599","ext_expires_in":"0","expires_on":"1489416293","not_before":"1489412393","resource":"https://graph.microsoft.com","access_token":"ey_-9Nv_","refresh_token":"AQ-0_"}"""
-type BodyPostTokenRequest = JsonProvider<bodySample>
+type OAuth2Token = JsonProvider<bodySample>
 
 let Run(myTimer: TimerInfo, log: TraceWriter) =
     let appSettings = ConfigurationManager.AppSettings 
@@ -29,6 +30,6 @@ let Run(myTimer: TimerInfo, log: TraceWriter) =
 
     log.Info(sprintf "Message sent: %s" postInformation)
 
-    let body = Http.RequestString(url, headers = [ ContentType HttpContentTypes.FormValues ], body = TextRequest postInformation) |> BodyPostTokenRequest.Parse  
+    let token = Http.RequestString(url, headers = [ ContentType HttpContentTypes.FormValues ], body = TextRequest postInformation) |> OAuth2Token.Parse  
     
-    log.Info(sprintf "token: %s" body.AccessToken)
+    log.Info(sprintf "token: %s" token.AccessToken)
