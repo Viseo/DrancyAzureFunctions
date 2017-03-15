5235723c0dacecf205c6fc843ddb1bafcd44b7b4
allan.menoret <allan.menoret@gmail.com>
2017-03-15 11:19:57 +0100

    upgrade testing with testing file

--- get-test/run.fsx
+++ get-test/run.fsx
@@ -3,9 +3,11 @@
 open System.Linq
 open System.Net
 open System.Net.Http
+open System.IO
 
+[<Literal>]
+let sampleRate = __SOURCE_DIRECTORY__ + "/playlistSample.json"
 
 let Run(req: HttpRequestMessage, log: TraceWriter) =
-    let people = "Me !!!"
-
-    req.CreateResponse(HttpStatusCode.OK, sprintf "{%s}" people)
+    let content = File.ReadAllText(sampleRate)
+    req.CreateResponse(HttpStatusCode.OK, content)
