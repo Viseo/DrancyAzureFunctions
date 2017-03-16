7f8d97069ee529bf2cbd263b04f8cacba31e8901
allan.menoret <allan.menoret@gmail.com>
2017-03-16 15:54:02 +0100

    get event from calendar and format them

--- get-test/run.fsx
+++ get-test/run.fsx
@@ -19,10 +19,10 @@ open Microsoft.Azure.WebJobs.Host
 open Microsoft.Azure.WebJobs
 
 [<Literal>]
-let playlistSample = __SOURCE_DIRECTORY__ + "/playlistSample.json"
+let playlistSample = __SOURCE_DIRECTORY__ + "/Sample/playlistSample.json"
 
 [<Literal>]
-let calendarSample = __SOURCE_DIRECTORY__ + "/calendarSample.json"
+let calendarSample = __SOURCE_DIRECTORY__ + "/Sample/calendar.json"
 type OfficeCalendar = JsonProvider<calendarSample>
 
 [<Literal>]
@@ -35,19 +35,24 @@ let createResponse data =
     response.Content.Headers.ContentType <- MediaTypeHeaderValue("application/json")
     response
 
-let getPlaylistFromCalendar token (log: TraceWriter) =
+let createPlaylist name (playlists: OfficeCalendar.Root) =
+    let events = playlists.Value |> Seq.filter (fun event -> event.Organizer.EmailAddress.Address = name || (event.Attendees |> Seq.exists (fun a -> a.EmailAddress.Address = name)))
+                            |> Seq.map (fun event -> "{" + "UniqueId = " + event.Id + ", Content = " + event.Location.DisplayName + "}")
+                            |> String.concat ","
+    """{"Groups":[{"UniqueId":"audio_video_picture","Title":"Universal Media Player Tests","Category":"Windows 10 Universal Media Player Tests","ImagePath": "ms-appx:///Assets/AudioVideo.png","Description":"Windows 10 Universal Media Player Tests","Items": [""" + events + "]}]}"
+
+let getPlaylistFromCalendar name token (log: TraceWriter) =
     let now = DateTime.Now
     let url = "https://graph.microsoft.com/v1.0/me/calendar/calendarView?startDateTime=" + now.ToString("yyyy-MM-ddTHH:mm:ss.fffffff") + "&endDateTime=" + now.AddSeconds(1.).ToString("yyyy-MM-ddTHH:mm:ss.fffffff")
-    log.Info(sprintf "url: %s" url)
-    log.Info(sprintf "token: %s" token)
     let playlists = Http.RequestString(url, headers = [ "Authorization", "Bearer " + token])
     log.Info(sprintf "playlists: %s" playlists)
-    File.ReadAllText(playlistSample)
+    playlists |> OfficeCalendar.Parse |> createPlaylist name
+    //File.ReadAllText(playlistSample)
 
-let getPlaylist (log: TraceWriter) =
+let getPlaylist name (log: TraceWriter) =
     match File.ReadAllText(tokenFile) with
     | "null" -> File.ReadAllText(playlistSample)
-    | token -> getPlaylistFromCalendar token log
+    | token -> getPlaylistFromCalendar name token log
 
-let Run(req: HttpRequestMessage, log: TraceWriter) =
-    getPlaylist log |> createResponse
+let Run(req: HttpRequestMessage, name: string, log: TraceWriter) =
+    getPlaylist name log |> createResponse
